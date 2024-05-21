8384df9b146666d5eaf78ff61ff0bf8147dc6c6c
Artur Reznikov <artur.reznikov@avalabs.org>
2024-05-21 14:08:17 -0400

    add node operations: - create - destroy - exec - post

---
+++ node/destroy.go
@@ -0,0 +1,9 @@
+// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package node
+
+// Destoy destroys a node.
+func Destoy(node Node) error {
+	return nil
+}
---
+++ node/post.go
@@ -0,0 +1,9 @@
+// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package node
+
+// Post sends a POST request to the node at the specified path with the provided body.
+func Post(node Node, path string, body string) error {
+	return nil
+}
