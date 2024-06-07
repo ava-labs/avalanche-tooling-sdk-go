0b5f16a7d68c0fea9f86ee8b3fab3d51aa7c19f0
Artur Reznikov <artur.reznikov@avalabs.org>
2024-06-07 11:56:07 -0700

    lint

--- host/utils_test.go
+++ host/utils_test.go
@@ -7,13 +7,13 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetDefaultProjectNameFromGCPCredentials(t *testing.T) {
 	// Create a temporary file to act as the credentials file
 	tempFile, err := os.CreateTemp("", "credentials-*.json")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer os.Remove(tempFile.Name())
 
 	// Write a sample JSON configuration to the temporary file
@@ -26,29 +26,29 @@ func TestGetDefaultProjectNameFromGCPCredentials(t *testing.T) {
 		"type": "service_account"
 	}`
 	_, err = tempFile.WriteString(sampleConfig)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	_ = tempFile.Close()
 
 	// Test the function with a valid credentials file
 	projectID, err := getDefaultProjectNameFromGCPCredentials(tempFile.Name())
-	assert.NoError(t, err)
-	assert.Equal(t, "test-project-id", projectID)
+	require.NoError(t, err)
+	require.Equal(t, "test-project-id", projectID)
 
 	// Test the function with a non-existent file
 	_, err = getDefaultProjectNameFromGCPCredentials("nonexistent-file.json")
-	assert.Error(t, err)
+	require.Error(t, err)
 
 	// Test the function with an invalid JSON file
 	invalidFile, err := os.CreateTemp("", "invalid-*.json")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer os.Remove(invalidFile.Name())
 
 	_, err = invalidFile.WriteString("invalid json")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	_ = invalidFile.Close()
 
 	_, err = getDefaultProjectNameFromGCPCredentials(invalidFile.Name())
-	assert.Error(t, err)
+	require.Error(t, err)
 }
 
 func TestGetPublicKeyFromSSHKey(t *testing.T) {
@@ -59,7 +59,7 @@ func TestGetPublicKeyFromSSHKey(t *testing.T) {
 	testPublicKeyPath := tempDir + "/id_rsa.pub"
 	testPublicKeyContent := "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAr8E7T/ZoQ9Jyb5F1U1t/9F+nkRoSi8g8j6x0g7vZJ68dVVpREzK84+R5cOJ6ydP9Nd+G99kW1HLhfwK5BhJnW3uZ7h1mL0Hh/RZb8csViNe8sEc2FSgH5G8cl3ZX8Y1UtdbS4k5F3cC3B4JFF9y6vOZRwUBO4z1Z2BZaGP29sXXkW0ZGRrWaBswcq+S5FJ1QOeeJ38OjkB45L7zq2X2NQ== user@hostname"
 	err := os.WriteFile(testPublicKeyPath, []byte(testPublicKeyContent+"\n"), 0o600)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	// Test cases
 	tests := []struct {
@@ -87,10 +87,10 @@ func TestGetPublicKeyFromSSHKey(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GetPublicKeyFromSSHKey(tt.keyPath)
 			if tt.wantErr {
-				assert.Error(t, err)
+				require.Error(t, err)
 			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.expected, got)
+				require.NoError(t, err)
+				require.Equal(t, tt.expected, got)
 			}
 		})
 	}
