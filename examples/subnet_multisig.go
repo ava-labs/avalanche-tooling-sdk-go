7d56a10b369948d8491df2b948e5a781d01fcbef
Raymond Sukanto <rsukanto@umich.edu>
2024-06-18 13:00:47 -0400

    multisig

--- examples/subnet_multisig.go
+++ examples/subnet_multisig.go
@@ -1,76 +1,25 @@
 // Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package subnet
+package examples
 
 import (
 	"context"
 	"fmt"
-	"github.com/ava-labs/avalanchego/ids"
-	"math/big"
-	"testing"
-	"time"
-
 	"github.com/ava-labs/avalanche-tooling-sdk-go/avalanche"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/keychain"
-	"github.com/ava-labs/avalanche-tooling-sdk-go/vm"
+	"github.com/ava-labs/avalanche-tooling-sdk-go/subnet"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/wallet"
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 	"github.com/ava-labs/avalanchego/wallet/subnet/primary"
-	"github.com/ava-labs/subnet-evm/core"
-	"github.com/ava-labs/subnet-evm/params"
-	"github.com/ethereum/go-ethereum/common"
+	"time"
 )
 
-func getDefaultSubnetEVMGenesis() SubnetParams {
-	allocation := core.GenesisAlloc{}
-	defaultAmount, _ := new(big.Int).SetString(vm.DefaultEvmAirdropAmount, 10)
-	allocation[common.HexToAddress("INITIAL_ALLOCATION_ADDRESS")] = core.GenesisAccount{
-		Balance: defaultAmount,
-	}
-	return SubnetParams{
-		SubnetEVM: &SubnetEVMParams{
-			ChainID:     big.NewInt(123456),
-			FeeConfig:   vm.StarterFeeConfig,
-			Allocation:  allocation,
-			Precompiles: params.Precompiles{},
-		},
-		Name: "TestSubnet",
-	}
-}
-
-func TestSubnetDeploy(_ *testing.T) {
-	subnetParams := getDefaultSubnetEVMGenesis()
-	newSubnet, _ := New(&subnetParams)
-	network := avalanche.FujiNetwork()
-	keychain, _ := keychain.NewKeychain(network, "KEY_PATH")
-	controlKeys := keychain.Addresses().List()
-	subnetAuthKeys := keychain.Addresses().List()
-	threshold := 1
-	newSubnet.SetSubnetCreateParams(controlKeys, uint32(threshold))
-	wallet, _ := wallet.New(
-		context.Background(),
-		&primary.WalletConfig{
-			URI:              network.Endpoint,
-			AVAXKeychain:     keychain.Keychain,
-			EthKeychain:      secp256k1fx.NewKeychain(),
-			PChainTxsToFetch: nil,
-		},
-	)
-	deploySubnetTx, _ := newSubnet.CreateSubnetTx(wallet)
-	subnetID, _ := newSubnet.Commit(*deploySubnetTx, wallet, true)
-	fmt.Printf("subnetID %s \n", subnetID.String())
-	time.Sleep(2 * time.Second)
-	newSubnet.SetBlockchainCreateParams(subnetAuthKeys)
-	deployChainTx, _ := newSubnet.CreateBlockchainTx(wallet)
-	blockchainID, _ := newSubnet.Commit(*deployChainTx, wallet, true)
-	fmt.Printf("blockchainID %s \n", blockchainID.String())
-}
-
-func TestSubnetDeployMultiSig(_ *testing.T) {
+func DeploySubnetMultiSig() {
 	subnetParams := getDefaultSubnetEVMGenesis()
 	// Create new Subnet EVM genesis
-	newSubnet, _ := New(&subnetParams)
+	newSubnet, _ := subnet.New(&subnetParams)
 
 	network := avalanche.FujiNetwork()
 
@@ -135,21 +84,10 @@ func TestSubnetDeployMultiSig(_ *testing.T) {
 	if err != nil {
 		fmt.Errorf("error signing tx walletA: %w", err)
 	}
-	_, remainingSigners, err := deployChainTx.GetRemainingAuthSigners()
-	if err != nil {
-		fmt.Errorf("error getting remianing signers 1: %w", err)
-	}
-	fmt.Printf("remainingSigners %s \n", remainingSigners)
-	fmt.Printf("signing with wallet B \n")
+	fmt.Printf("signing with wallet B")
 	if err := walletB.P().Signer().Sign(context.Background(), deployChainTx.PChainTx); err != nil {
 		fmt.Errorf("error signing tx walletB: %w", err)
 	}
-	_, remainingSigners, err = deployChainTx.GetRemainingAuthSigners()
-	if err != nil {
-		fmt.Errorf("error getting remianing signers 2: %w", err)
-	}
-	fmt.Printf("remainingSigners %s \n", remainingSigners)
-	time.Sleep(2 * time.Second)
 
 	// since we are using the fee paying key as control key too, we can commit the transaction
 	// on chain immediately since the number of signatures has been reached
