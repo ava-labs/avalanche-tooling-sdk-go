2188f4f15c1c4e632cd10352536bc9f76984cb83
Artur Reznikov <artur.reznikov@avalabs.org>
2024-06-25 15:29:42 -0700

    shell lint

--- host/shell/setupDockerService.sh
+++ host/shell/setupDockerService.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Check if Docker is installed
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Please install Docker and try again."
-    exit 1
+if ! command -v docker &>/dev/null; then
+	echo "Docker is not installed. Please install Docker and try again."
+	exit 1
 fi
 
 # Check if Docker Compose is installed
-if ! docker compose version &> /dev/null; then
-    echo "Docker Compose is not installed or not configured correctly. Please install Docker Compose and try again."
-    exit 1
+if ! docker compose version &>/dev/null; then
+	echo "Docker Compose is not installed or not configured correctly. Please install Docker Compose and try again."
+	exit 1
 fi
 
 # Provide docker-compose systemctl unit file
-cat << EOF | sudo tee /etc/systemd/system/avalanche-cli-docker.service
+cat <<EOF | sudo tee /etc/systemd/system/avalanche-cli-docker.service
 [Unit]
 Description=Avalanche CLI Docker Compose Service
 Requires=docker.service
@@ -37,4 +37,3 @@ sudo systemctl daemon-reload
 sudo systemctl enable avalanche-cli-docker.service
 
 echo "Service created and enabled successfully."
-
