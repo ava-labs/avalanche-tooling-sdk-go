d18e86a9bdfe57f4999178e8f1e543477e14eae4
Artur Reznikov <artur.reznikov@avalabs.org>
2024-06-27 11:15:05 -0700

    add security key

--- utils/net.go
+++ utils/net.go
@@ -3,8 +3,47 @@
 
 package utils
 
-import "net"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net"
+	"net/http"
+)
 
 func IsValidIP(ipStr string) bool {
 	return net.ParseIP(ipStr) != nil
+}
+
+// GetUserIPAddress retrieves the IP address of the user.
+func GetUserIPAddress() (string, error) {
+	resp, err := http.Get("https://api.ipify.org?format=json")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("HTTP request failed")
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return "", err
+	}
+
+	ipAddress, ok := result["ip"].(string)
+	if ok {
+		if net.ParseIP(ipAddress) == nil {
+			return "", errors.New("invalid IP address")
+		}
+		return ipAddress, nil
+	}
+
+	return "", errors.New("no IP address found")
 }
