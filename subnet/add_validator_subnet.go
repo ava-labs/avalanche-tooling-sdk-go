723aa480913f186ebb038c141f8e99548bf97678
Raymond Sukanto <rsukanto@umich.edu>
2024-08-22 17:52:45 +0700

    update example

--- subnet/add_validator_subnet.go
+++ subnet/add_validator_subnet.go
@@ -5,6 +5,8 @@ package subnet
 
 import (
 	"errors"
+	"github.com/ava-labs/avalanchego/ids"
+	"time"
 )
 
 var (
@@ -13,6 +15,18 @@ var (
 	ErrEmptySubnetID          = errors.New("subnet ID is not provided")
 	ErrEmptySubnetAuth        = errors.New("no subnet auth keys is provided")
 )
+
+type ValidatorParams struct {
+	// NodeID is the unique identifier of the node to be added as a validator on the specified Subnet.
+	NodeID ids.NodeID
+	// Duration is how long the node will be staking the Subnet
+	// Duration has to be less than or equal to the duration that the node will be validating the Primary
+	// Network
+	Duration time.Duration
+	// Weight is the validator's weight when sampling validators.
+	// Weight for subnet validators is set to 20 by default
+	Weight uint64
+}
 
 // AddValidator adds validator to subnet
 // Before an Avalanche Node can be added as a validator to a Subnet, the node must already be
