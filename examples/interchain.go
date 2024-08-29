3557b0a2efc50d67de02434a05279a1e99487deb
Felipe Madero <felipe.madero@gmail.com>
2024-08-28 22:48:40 -0300

    adding relayer conf generation

--- examples/interchain.go
+++ examples/interchain.go
@@ -11,60 +11,88 @@ import (
 	"github.com/ava-labs/avalanche-tooling-sdk-go/avalanche"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/interchain/icm"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/interchain/relayer"
+	"github.com/ava-labs/avalanche-tooling-sdk-go/key"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/utils"
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/logging"
 )
-
-func fatal(err error) {
-	fmt.Println(err)
-	os.Exit(1)
-}
 
 func main() {
 	icmVersion, err := icm.GetLatestVersion()
 	if err != nil {
-		fatal(err)
+		panic(err)
 	}
 	td := icm.Deployer{}
 	if err := td.DownloadAssets(icmVersion); err != nil {
-		fatal(err)
+		panic(err)
 	}
+	chain1RPC := os.Getenv("CHAIN1_RPC")
+	chain1PK := os.Getenv("CHAIN1_PK")
+	chain2RPC := os.Getenv("CHAIN2_RPC")
+	chain2PK := os.Getenv("CHAIN2_PK")
 	chain1MessengerAlreadyDeployed, chain1MessengerAddress, chain1RegistryAddress, err := td.Deploy(
-		os.Getenv("CHAIN1_RPC"),
-		os.Getenv("CHAIN1_PK"),
+		chain1RPC,
+		chain1PK,
 		true,
 		true,
 	)
 	if err != nil {
-		fatal(err)
+		panic(err)
+	}
+	if !chain1MessengerAlreadyDeployed {
+		panic(fmt.Errorf("icm already deployed to %s", chain1RPC))
 	}
 	chain2MessengerAlreadyDeployed, chain2MessengerAddress, chain2RegistryAddress, err := td.Deploy(
-		os.Getenv("CHAIN2_RPC"),
-		os.Getenv("CHAIN2_PK"),
+		chain2RPC,
+		chain2PK,
 		true,
 		true,
 	)
 	if err != nil {
-		fatal(err)
+		panic(err)
 	}
+	if !chain2MessengerAlreadyDeployed {
+		panic(fmt.Errorf("icm already deployed to %s", chain2RPC))
+	}
+
 	chain1RegistryAddress = "0x4bC756894C6CB10A5735816E25132486F5a1cE8f"
 	chain2RegistryAddress = "0x302a91b43d974Cd6f12f4Eae8cADBc8efB7359c8"
-	fmt.Println(chain1MessengerAlreadyDeployed)
-	fmt.Println(chain1MessengerAddress)
-	fmt.Println(chain1RegistryAddress)
-	fmt.Println(chain2MessengerAlreadyDeployed)
-	fmt.Println(chain2MessengerAddress)
-	fmt.Println(chain2RegistryAddress)
 
 	network := avalanche.FujiNetwork()
 
 	relayerDir := os.Getenv("RELAYER_DIR")
 	if relayerDir == "" {
-		fatal(fmt.Errorf("must define RELAYER_DIR env var"))
+		panic(fmt.Errorf("must define RELAYER_DIR env var"))
 	}
 	relayerDir = utils.ExpandHome(relayerDir)
 	if !utils.DirectoryExists(relayerDir) {
-		fatal(fmt.Errorf("relayer directory %q does not exits", relayerDir))
+		panic(fmt.Errorf("relayer directory %q does not exits", relayerDir))
+	}
+
+	chain1RelayerKey, err := key.NewSoft()
+	if err != nil {
+		panic(err)
+	}
+	chain2RelayerKey, err := key.NewSoft()
+	if err != nil {
+		panic(err)
+	}
+
+	chain1SubnetID, err := ids.FromString(os.Getenv("CHAIN1_SUBNET_ID"))
+	if err != nil {
+		panic(err)
+	}
+	chain1BlockchainID, err := ids.FromString(os.Getenv("CHAIN1_BLOCKCHAIN_ID"))
+	if err != nil {
+		panic(err)
+	}
+	chain2SubnetID, err := ids.FromString(os.Getenv("CHAIN2_SUBNET_ID"))
+	if err != nil {
+		panic(err)
+	}
+	chain2BlockchainID, err := ids.FromString(os.Getenv("CHAIN2_BLOCKCHAIN_ID"))
+	if err != nil {
+		panic(err)
 	}
 
 	relayerConfig := relayer.CreateBaseRelayerConfig(
@@ -73,6 +101,30 @@ func main() {
 		0,
 		network,
 	)
-	fmt.Printf("%#v\n", relayerConfig)
-
+	relayer.AddBlockchainToRelayerConfig(
+		relayerConfig,
+		chain1RPC,
+		"",
+		chain1SubnetID,
+		chain1BlockchainID,
+		chain1RegistryAddress,
+		chain1MessengerAddress,
+		chain1RelayerKey.C(),
+		chain1RelayerKey.PrivKeyHex(),
+	)
+	relayer.AddBlockchainToRelayerConfig(
+		relayerConfig,
+		chain2RPC,
+		"",
+		chain2SubnetID,
+		chain2BlockchainID,
+		chain2RegistryAddress,
+		chain2MessengerAddress,
+		chain2RelayerKey.C(),
+		chain2RelayerKey.PrivKeyHex(),
+	)
+	relayerConfigPath := filepath.Join(relayerDir, "config.json")
+	if err := relayer.SaveRelayerConfig(relayerConfig, relayerConfigPath); err != nil {
+		panic(err)
+	}
 }
