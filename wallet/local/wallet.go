cf23ae8b47f3930355f5edeee164047dc8aa31f9
Raymond Sukanto <rsukanto@umich.edu>
2025-10-14 17:52:23 -0400

    lint

--- wallet/local/wallet.go
+++ wallet/local/wallet.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/ava-labs/avalanchego/ids"
-
 	"github.com/ava-labs/avalanchego/wallet/subnet/primary"
 	"github.com/ava-labs/avalanchego/wallet/subnet/primary/common"
 
