6dd064dddd9de22541a7deb5a8632c34eb25eba3
Felipe Madero <felipe.madero@gmail.com>
2025-11-10 15:50:03 -0300

    Sync with wallet-refactor-primary-operations-new

--- wallet/chains/pchain/sender.go
+++ wallet/chains/pchain/sender.go
@@ -3,75 +3,27 @@
 package pchain
 
 import (
-	"errors"
 	"fmt"
-	"time"
 
-	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
 	"github.com/ava-labs/avalanchego/wallet/subnet/primary"
-	"github.com/ava-labs/avalanchego/wallet/subnet/primary/common"
 
-	"github.com/ava-labs/avalanche-tooling-sdk-go/multisig"
-	"github.com/ava-labs/avalanche-tooling-sdk-go/utils"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/wallet/types"
-)
 
-// sendWithRetry submits a transaction to the network with retry logic
-func sendWithRetry(wallet *primary.Wallet, tx *txs.Tx) error {
-	const (
-		repeats             = 3
-		sleepBetweenRepeats = 2 * time.Second
-	)
-	var issueTxErr error
-	for i := 0; i < repeats; i++ {
-		ctx, cancel := utils.GetAPILargeContext()
-		defer cancel()
-		options := []common.Option{common.WithContext(ctx)}
-		// TODO: split error checking and recovery between issuing and waiting for status
-		issueTxErr = wallet.P().IssueTx(tx, options...)
-		if issueTxErr == nil {
-			break
-		}
-		if ctx.Err() != nil {
-			issueTxErr = fmt.Errorf("timeout issuing/verifying tx with ID %s: %w", tx.ID(), issueTxErr)
-		} else {
-			issueTxErr = fmt.Errorf("error issuing tx with ID %s: %w", tx.ID(), issueTxErr)
-		}
-		time.Sleep(sleepBetweenRepeats)
-	}
-	if issueTxErr != nil {
-		return fmt.Errorf("issue tx error %w", issueTxErr)
-	}
-	return nil
-}
+	avagoTxs "github.com/ava-labs/avalanchego/vms/platformvm/txs"
+)
 
 // SendTx sends P-Chain transactions
 func SendTx(wallet *primary.Wallet, params types.SendTxParams) (types.SendTxResult, error) {
-	// Validate transaction is defined
-	if params.SignTxResult.Undefined() {
-		return types.SendTxResult{}, multisig.ErrUndefinedTx
+	// Get the P-Chain transaction from the SignTxResult
+	pChainTx, ok := params.SignTxResult.GetTx().(*avagoTxs.Tx)
+	if !ok {
+		return types.SendTxResult{}, fmt.Errorf("expected P-Chain transaction, got %T", params.SignTxResult.GetTx())
 	}
 
-	// Validate transaction is ready to commit
-	isReady, err := params.SignTxResult.IsReadyToCommit()
-	if err != nil {
-		return types.SendTxResult{}, err
-	}
-	if !isReady {
-		return types.SendTxResult{}, errors.New("tx is not fully signed so can't be committed")
-	}
-
-	// Extract the P-Chain transaction
-	tx, err := params.SignTxResult.GetWrappedPChainTx()
-	if err != nil {
-		return types.SendTxResult{}, err
-	}
-
-	// Submit the signed transaction to the network with retry logic
-	if err := sendWithRetry(wallet, tx); err != nil {
+	// Submit the signed transaction to the network
+	if err := wallet.P().IssueTx(pChainTx); err != nil {
 		return types.SendTxResult{}, fmt.Errorf("error sending tx: %w", err)
 	}
-
-	pChainSendResult := types.NewPChainSendTxResult(tx)
+	pChainSendResult := types.NewPChainSendTxResult(pChainTx)
 	return types.SendTxResult{SendTxOutput: pChainSendResult}, nil
 }
---
+++ wallet/signer.go
@@ -0,0 +1,32 @@
+// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package wallet
+
+import (
+	"fmt"
+
+	"github.com/ava-labs/avalanchego/wallet/subnet/primary"
+
+	"github.com/ava-labs/avalanche-tooling-sdk-go/wallet/chains/pchain"
+	"github.com/ava-labs/avalanche-tooling-sdk-go/wallet/types"
+)
+
+// SignTx constructs a transaction for the specified operation
+func SignTx(wallet *primary.Wallet, params types.SignTxParams) (types.SignTxResult, error) {
+	// Validate parameters first
+	if err := params.Validate(); err != nil {
+		return types.SignTxResult{}, fmt.Errorf("invalid parameters: %w", err)
+	}
+
+	// Route to appropriate chain handler based on chain type
+	switch chainType := params.GetChainType(); chainType {
+	case pchain.ChainType:
+		result, err := pchain.SignTx(wallet, params)
+		if err != nil {
+			return types.SignTxResult{}, err
+		}
+		return types.SignTxResult{SignTxOutput: result.SignTxOutput}, nil
+	default:
+		return types.SignTxResult{}, fmt.Errorf("unsupported chain type: %s", chainType)
+	}
+}
